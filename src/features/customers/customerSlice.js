d5e42f8a036d6bfce7145b12f4f68a9bd227258e
shohan11d <shohan11d@gmail.com>
2024-09-09 12:43:30 +0600

    10. legacy connect components to redux

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,43 @@
+
+const initialStateCustomer = {
+  fullName: '',
+  nationalId: '',
+  createdAt: '',
+};
+
+
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payLoad.fullName,
+        nationalID: action.payLoad.nationalID,
+        createdAt: action.payLoad.createdAt,
+      };
+
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payLoad,
+      };
+    default:
+      return state;
+  }
+}
+
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payLoad: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return {
+    type: 'account/updateName',
+    payLoad: fullName,
+  };
+}
