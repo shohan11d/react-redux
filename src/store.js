781c485a440fb8bd4fda6733b128cea3a542619f
shohan11d <shohan11d@gmail.com>
2024-09-09 18:59:50 +0600

    12. Api call with redux thunks

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { type } from '@testing-library/user-event/dist/type';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import accountReducer from './features/accounts/accountslice';
 import customerReducer from './features/customers/customerSlice';
 
@@ -7,6 +8,6 @@ const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
